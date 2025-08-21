b5ea772d548ce9650133dec13a34dff2ef53890d
Aryan <aryan.gholamlou@gmail.com>
2025-08-21 23:30:02 +0330

    Init project.

---
+++ src/main/kotlin/org/example/moonstyle/MoonStyleApplication.kt
@@ -0,0 +1,11 @@
+package org.example.moonstyle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoonStyleApplication
+
+fun main(args: Array<String>) {
+    runApplication<MoonStyleApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/moonstyle/MoonStyleApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.moonstyle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoonStyleApplicationTests {
+    
+    @Test
+    fun contextLoads() {
+    }
+    
+}
